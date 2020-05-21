9964eed9864c4e5e13e409e1cb2a735fb400218e
B <jayagurusurya.b@capgemini.com>
2020-05-21 12:33:02 +0530

    ajax included

--- src/js/app.js
+++ src/js/app.js
@@ -2,3 +2,20 @@ import '../css/main.scss';
 import '../fonts/verdana/stylesheet.css'
 import{ HeaderComponent } from './header.component';
 import{ ShoppingListComponent } from './shopping-list.component';
+
+
+
+    const xhttp = new XMLHttpRequest();
+    let shopingList;
+    xhttp.onload = function(){
+        if(this.readyState == 4 && this.status == 200){
+            shopingList = this.responseText;
+            console.log('reslist',shopingList);
+        }
+        else{
+            alert('json payload failure');
+        }
+    }
+    xhttp.open('get','../../src/json/list.json');
+    xhttp.send();
+    
